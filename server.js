a9cf662f82a8ddf8bc9eaeb9eed9b67b1d08e788
eelaukka <eelaukka@student.oulu.fi>
2017-10-17 14:45:50 +0300

    goeasy3

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.listen(process.env.PORT || 8080, function () {
 app.use(express.static('public'))
 //
 app.get('/', function (req, res) {    
-    res.send("goeasy2");
+    res.send("goeasy3");
 })
 
 
