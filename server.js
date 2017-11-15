1a654b5804d2bc33f4b9a8a0bd0b40b934b303aa
eelaukka <eelaukka@student.oulu.fi>
2017-11-15 15:08:19 +0200

    create user with avatar

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require('express')
 const app = express()
+const {
+    Client
+} = require('pg');
 
+const client = new Client({
+    connectionString: process.env.DATABASE_URL || "postgres://xjcnfjccvbpepn:198cb597d44207d8239ae13641a103e7c47617566775bf0b73869923a841305d@ec2-54-75-225-143.eu-west-1.compute.amazonaws.com:5432/dai971uulqhd0f?ssl=true",
+});
 
+client.connect();
+//app.get('/api/user', function(req,res) {
+//client.query('SELECT * FROM user_table;', (err, res) => {
+//    if (err) throw err;
+//    for (let row of res.rows) 
+//    {
+//        console.log(JSON.stringify(row.username));
+//        console.log(JSON.stringify(row.mpassword));
+//    }
+//    //client.end();
+//});
+//});
 
 
 app.listen(process.env.PORT || 8080, function () {
@@ -9,16 +27,26 @@ app.listen(process.env.PORT || 8080, function () {
 })
 app.use(express.static('public'))
 //
-app.get('/', function (req, res) {    
+app.get('/', function (req, res) {
     res.send("goeasy3");
 })
 
 
-app.post('/createaccount', function (req, res) {    
-    if (!req.body.username || !req.body.password) {
-        return; 
-    } else{
-        //
+app.post('/createuser', function (req, res) {
+    if (!req.body.loginname || !req.body.password) {
+        return;
+    } else {
+        var mAvatar = req.body.avatar;
+        var mLoginname = req.body.loginname;
+        var mPassHash = req.body.password;
+        client.query(`INSERT INTO user_table (username,mpassword,avatar) VALUES ('${mLoginname}', '${mPassHash}', '${mAvatar}');`, (err, res) => {
+            if (err) throw err;
+            for (let row of res.rows) {
+                console.log(JSON.stringify(row.username));
+                console.log(JSON.stringify(row.mpassword));
+            }
+            //client.end();
+        });
     }
     res.send("success");
 })
@@ -30,32 +58,32 @@ app.get('/login', function (req, res) {
     }
     res.send("success");
 })
-app.get('/tasks/:username', function (req, res) {    
-    tasks="hh";
+app.get('/tasks/:username', function (req, res) {
+    tasks = "hh";
     res.send("json");
-    
+
 })
-app.post('/createtask', function (req, res) {    
+app.post('/createtask', function (req, res) {
     if (!req.body.id || !req.body.title || !req.body.date) {
         return;
-    } else{
-        
+    } else {
+
     }
     res.send("success");
 })
-app.post('/modifytask', function (req, res) {    
+app.post('/modifytask', function (req, res) {
     if (!req.body.id || !req.body.title || !req.body.date) {
         return;
-    } else{
-        
+    } else {
+
     }
     res.send("success");
 })
-app.delete('/deletetask', function (req, res) {    
+app.delete('/deletetask', function (req, res) {
     if (!req.body.id || !req.body.token) {
         return;
-    } else{
-        
+    } else {
+
     }
     res.send("success");
 })
