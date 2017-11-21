25aab3ae084cc6d438c7932b5d4dbcb242973744
eelaukka <eelaukka@student.oulu.fi>
2017-11-21 14:19:59 +0200

    login/createuser

--- server.js
+++ server.js
@@ -27,27 +27,43 @@ app.get('/highscores', function (request, response) {
 });
 
 app.post("/createuser", function (req, res) {
-    let score = req.body;
-    console.log(score);
-    if (!req.body.loginname || !req.body.password) {
+    let reqJSON = req.body;
+    let mUser = reqJSON.loginname;
+    let mPassword = reqJSON.password;
+
+
+    if (!mUser || !mPassword) {
         return;
     } else {
-        var mAvatar = req.body.avatar;
-        var mLoginname = req.body.loginname;
-        var mPassHash = req.body.password;
-        client.query(`INSERT INTO user_table (username,mpassword,avatar) VALUES ('${mLoginname}', '${mPassHash}', '${mAvatar}');`, (err, res) => {
-            if (err) throw err;
-            for (let row of res.rows) {
-                console.log(JSON.stringify(row.username));
-                console.log(JSON.stringify(row.mpassword));
-            }
-            //client.end();
-        });
+        try {
+            var mAvatar = req.body.avatar;
+            client.query(`INSERT INTO user_table (username,mpassword,avatar) VALUES ('${mUser}', '${mPassword}', '${mAvatar}');`, (err, ress) => {
+                if (err) {
+                    res.status(200).send(JSON.stringify({
+                        msg: "Username already exists!"
+                    }));
+
+                } else {
+                    res.status(200).send(JSON.stringify({
+                        msg: "Your account has been created successfully."
+                    }));
+                }
+//                for (let row of ress.rows) {
+//                    console.log(JSON.stringify(row.username));
+//                    console.log(JSON.stringify(row.mpassword));
+//                }
+                //client.end();
+            });
+
+
+
+        } catch (err) {
+            res.status(100).send(JSON.stringify({
+                msg: "Username already exists"
+            }));
+        }
     }
-    res.status(200).send(JSON.stringify({
-        msg: "success"
-    }));
-})
+});
 
 
 app.use(function (err, req, res, next) {
@@ -60,25 +76,7 @@ app.listen(app.get('port'), function () {
 });
 
 
-function getHighScores() {
+function createUser() {
 
-
-}
-
-function saveHighScores(score) {
-
-    let client = new Client({
-        connectionString: process.env.DATABASE_URL,
-        ssl: true
-    });
-
-    client.connect();
-
-    let sql = `INSERT INTO "highscores"("name", "score", "timestamp") VALUES('${score.name}', ${score.score}, '${score.timestamp}') RETURNING "id", "name", "score", "timestamp";`
-
-    client.query(sql, (err, resp) => {
-        console.dir(resp);
-        client.end();
-    });
 
 }
